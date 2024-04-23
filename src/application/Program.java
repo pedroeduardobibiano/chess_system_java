dab0ddd088a62d50f8c0ddf1171f2cf2890f1e2e
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-04-23 19:08:23 -0300

    Starting to implement Board Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
+import bordgame.Board;
 import bordgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Position position = new Position(3, 5);
-		
-		System.out.println(position);
+		Board board = new Board(8, 8);
 		
 	}
 
